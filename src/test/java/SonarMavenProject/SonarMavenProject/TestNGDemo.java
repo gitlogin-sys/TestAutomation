e8f52873f3a63b9c2f2e5986e359aa990c0070d9
itengineersaini@gmail.com <itengineersaini@gmail.com>
2021-09-11 13:42:58 +0530

    added

--- src/test/java/SonarMavenProject/SonarMavenProject/TestNGDemo.java
+++ src/test/java/SonarMavenProject/SonarMavenProject/TestNGDemo.java
@@ -12,19 +12,28 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
+import TestMavenProject.TestDemoProject;
+
 public class TestNGDemo {
 @Test
 public void test01() throws InterruptedException, IOException{
-	//DemoJava d= new DemoJava();
-	//d.print();
+
 	LoginForm f= new LoginForm();
 	f.main(null);	
+	System.out.println("Runnig Test case 1...");
 }
-/*@Test
+@Test
 public void test02() throws InterruptedException, IOException{
 	DemoJava d= new DemoJava();
 	d.print();
+	System.out.println("Runnig Test case 2...");
+}
+
+@Test
+public void test03() throws InterruptedException, IOException{
+	TestDemoProject d= new TestDemoProject();
 	
-}*/
+	System.out.println("Runnig Test case 3...");
+}
 
 }
