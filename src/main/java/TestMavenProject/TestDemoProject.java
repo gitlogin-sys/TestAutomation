92283bcc4de93159d44631d08c4ac60504f51f0c
itengineersaini@gmail.com <itengineersaini@gmail.com>
2021-09-11 13:43:26 +0530

    i wq: Pddedlease enter the commit message for your changes. Lines starting

---
+++ src/main/java/TestMavenProject/TestDemoProject.java
@@ -0,0 +1,16 @@
+package TestMavenProject;
+
+public class TestDemoProject {
+	
+	
+public static void main(String[] args) {
+		
+		
+		System.out.println("Hello test demo...");
+		
+		
+
+	}
+
+
+}
